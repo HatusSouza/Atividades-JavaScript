2fc307f2972ddbb2c144b244ee30549035fed920
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-10-17 18:19:34 -0300

    Add files via upload

---
+++ 11/11.js
@@ -0,0 +1,16 @@
+function VerificarMedia(){
+    let media = document.querySelector("#media").value;
+    let mensagem ="";
+
+    if(media == 10){
+        mensagem="Aprovado com Sucesso" 
+    }
+    else if (media >= 7){
+        mensagem="Aprovado"
+    }
+    else if(media <= 7){
+        mensagem="Reprovado"
+    }
+
+    document.querySelector("#resposta").innerHTML = mensagem
+}
