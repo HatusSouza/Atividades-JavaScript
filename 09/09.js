dd3a7f1abe7f9d70ddbfffaad8c5cb8bf2c4abdf
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-10-17 17:24:38 -0300

    Add files via upload

---
+++ 09/09.js
@@ -0,0 +1,9 @@
+function PositivoNegativo(){
+    let numero = document.querySelector("#n").value
+
+    if(numero>0){
+        document.querySelector("#resposta").innerHTML = "Possitivo";}else{
+            document.querySelector("#resposta").innerHTML = "Negativo"}
+}
+
+    
