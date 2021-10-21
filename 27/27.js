f8709004002a9e3e31b470196ec4af4329609f88
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-10-21 14:41:13 -0300

    Add files via upload

---
+++ 27/27.js
@@ -0,0 +1,7 @@
+function TabelaPao(){
+    let precoUnitario=document.querySelector("#preco").value
+    document.querySelector("#resposta").innerHTML=""
+    for(let i=1;i<=50;i++){
+        document.querySelector("#resposta").innerHTML+=i+"-R$"+(precoUnitario*i)+"<br>"
+    }
+}
