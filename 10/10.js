d78d1b2c2ae54e211b3d61d74ebb1c0d19784bd9
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-10-21 16:19:58 -0300

    Add files via upload

---
+++ 10/10.js
@@ -0,0 +1,20 @@
+function VerificarTamanho(){
+    let tamanho=document.querySelector("#tamanho").value
+    let tamanhoTexto=""
+
+    if(tamanho.toUpperCase()=="G"){
+        tamanhoTexto="G - Grande"
+    }
+    else if(tamanho.toUpperCase()=="M"){
+        tamanhoTexto="M - Média"
+
+    }
+  else if(tamanho=="p"){
+      tamanhoTexto= "p - pequena"
+  }
+  else{
+        tamanhoTexto="Tamanho Invalido"}
+
+
+    document.querySelector("#resposta").innerHTML= " Tamanho Selicionado " + tamanhoTexto
+}
